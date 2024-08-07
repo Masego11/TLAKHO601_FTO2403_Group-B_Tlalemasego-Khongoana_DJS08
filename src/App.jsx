7f00cd647b9665255e98556c9df2ac194a8edfe0
Tlalemasego <tlalemasego@gmail.com>
2024-08-07 09:19:12 +0200

    git commit - Created my Layout and Header files and nested my routes inside a parent route, rendering my outlet content by using the outlet component.

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,24 @@
 
-import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import { BrowserRouter, Routes, Route} from 'react-router-dom';
 import Home from "./components/Home";
 import About from "./components/About";
 import Vans from "./components/Vans";
 import "./index.css";
 import VanDetail from './components/VansDetail.jsx';
 import "./server.js";
+import Layout from './components/Layout.jsx';
 
 
 function App() {
   return (
     <BrowserRouter>
-    <header>
-      <Link className="site-logo" to="/">#VanLife</Link>
-      <nav>
-        <Link to="/about">About</Link>
-        <Link to="/vans">Vans</Link>
-        </nav>
-        </header>
         <Routes>
+          <Route element={<Layout />}> 
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
           <Route path="/vans" element={<Vans />} />
           <Route path="/vans/:id" element={<VanDetail />} />
+          </Route>
         </Routes>
       </BrowserRouter>
   )
