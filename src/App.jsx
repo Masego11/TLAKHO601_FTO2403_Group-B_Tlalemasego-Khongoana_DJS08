16b395d2c3d2ff76cee78a5da1c53739dd9578d6
Tlalemasego <tlalemasego@gmail.com>
2024-08-07 08:50:39 +0200

    git commit - Created a link between van and van id and grabbed params from the url to make a fetch request to get the van with specific id

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Home from "./components/Home";
 import About from "./components/About";
 import Vans from "./components/Vans";
 import "./index.css";
+import VanDetail from './components/VansDetail.jsx';
 import "./server.js";
 
 
@@ -21,6 +22,7 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
           <Route path="/vans" element={<Vans />} />
+          <Route path="/vans/:id" element={<VanDetail />} />
         </Routes>
       </BrowserRouter>
   )
