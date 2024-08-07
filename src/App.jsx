3cd026ff1e14292f815e8b4932e85d8e968c4f4d
Tlalemasego <tlalemasego@gmail.com>
2024-08-07 15:46:30 +0200

    git commit - Created host van details' navbar

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,9 @@ import Reviews from "./pages/Host/Reviews.jsx";
 import HostLayout from './components/HostLayout.jsx';
 import HostVanDetail from './pages/Host/HostVanDetail.jsx';
 import HostVans from './pages/Host/HostVans.jsx';
+import HostVanInfo from './pages/Host/HostVanInfo.jsx';
+import HostVanPhotos from './pages/Host/HostVanPhotos.jsx';
+import HostVanPricing from './pages/Host/HostVanPricing.jsx';
 import "./index.css";
 import "./server.js";
 
@@ -32,7 +35,11 @@ function App() {
             <Route path="income" element={<Income />} />
             <Route path="reviews" element={<Reviews />} />
             <Route path="vans" element={<HostVans />} />
-            <Route path="vans/:id" element={<HostVanDetail />} />
+            <Route path="vans/:id" element={<HostVanDetail />}>
+            <Route index element={<HostVanInfo />} />
+            <Route path="pricing" element={<HostVanPricing />} />
+            <Route path="photos" element={<HostVanPhotos />} />
+            </Route>
           </Route>
         </Route>
       </Routes>
---
+++ src/pages/Host/HostVanPricing.jsx
@@ -0,0 +1,8 @@
+
+function HostVanPricing() {
+    return (
+        <h2>Detailed info goes here</h2>
+    )
+}
+
+export default HostVanPricing
